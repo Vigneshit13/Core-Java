3b73f022dd05ae7852e415836643c9cb79699c75
Vignesh <vigneshit13@gmail.com>
2018-02-03 15:03:40 +0530

    Pattern Triangle program

--- pattern3.java
+++ pattern3.java
@@ -1,7 +1,7 @@
 public class pattern3{
 public static void main(String args[]){
 	int n=5;
-	int z=1,y=1;
+	int z=1,y=1,x=1;
 	for(int i=1;i<=n;i++){
 		for(int j=n-1;j>=i;j--){
 		System.out.print("  ");
@@ -22,6 +22,16 @@ for(int k=1;k<=y;k++){
 System.out.print(k+" ");
 }
 y++;
+System.out.println();
+}
+
+for(int i=1;i<=n;i++){
+for(int j=n-1;j>=i;j--){
+System.out.print("  ");
+}
+for(int k=1;k<=x;k++){
+System.out.print((char)(k-1+65)+" ");
+}x++;
 System.out.println();
 }
 }
