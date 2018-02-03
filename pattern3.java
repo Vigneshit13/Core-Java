c49018789c85f2a219a1f164be256428a369a0f7
Vignesh <vigneshit13@gmail.com>
2018-02-03 12:40:05 +0530

    Pattern Programs

---
+++ pattern3.java
@@ -0,0 +1,28 @@
+public class pattern3{
+public static void main(String args[]){
+	int n=5;
+	int z=1,y=1;
+	for(int i=1;i<=n;i++){
+		for(int j=n-1;j>=i;j--){
+		System.out.print("  ");
+		}
+	for(int k=1;k<=z;k++){
+	System.out.print("* ");
+		}
+	z++;
+	System.out.println();
+	}
+
+for(int i=1;i<=n;i++){
+for(int j=n-1;j>=i;j--){
+System.out.print("  ");
+}
+for(int k=1;k<=y;k++){
+
+System.out.print(k+" ");
+}
+y++;
+System.out.println();
+}
+}
+}
