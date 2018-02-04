8dd555cd478f7dea2c3fa4bf92cd482eb1282017
Vignesh <vigneshit13@gmail.com>
2018-02-04 07:27:35 +0530

    Pattern Example

--- pattern4.java
+++ pattern4.java
@@ -22,5 +22,30 @@ System.out.print((char)(k+64)+" ");
 y--;
 System.out.println();
 }
+//Print numbers in pyramid
+int size=5,z1=1;
+
+for(int i=0;i<size;i++){
+for(int j=size-1;j>i;j--){
+System.out.print("  ");
+}
+for(int k=0;k<z1;k++){
+System.out.print(z1+" ");
+}
+z1+=2;
+System.out.println();
+}
+int z2=1;
+for(int i=0;i<size;i++){
+for(int j=size-1;j>i;j--){
+System.out.print("  ");
+}
+for(int k=0;k<z2;k++){
+System.out.print((z2-i)+" ");
+}
+z2+=2;
+System.out.println();
+}
+
 }
 }
