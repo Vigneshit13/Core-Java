c46ed649df153475649013813376f5ecdc5121d9
Vignesh <vigneshit13@gmail.com>
2018-02-04 07:35:33 +0530

    Pattern Exapmle

--- pattern4.java
+++ pattern4.java
@@ -47,5 +47,16 @@ z2+=2;
 System.out.println();
 }
 
+int m=1;
+for(int i=0;i<size;i++){
+for(int j=size-1;j>i;j--){
+System.out.print("  ");
+}
+for(int k=0;k<m;k++){
+System.out.print((char)(i+65)+" ");
+}
+m+=2;
+System.out.println();
+}
 }
 }
