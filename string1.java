168979f68b9823cd949feedb5ee6907460041350
Vignesh <vigneshit13@gmail.com>
2018-03-07 21:17:19 +0530

    Strings Example

--- string1.java
+++ string1.java
@@ -2,12 +2,19 @@ import java.util.*;
 class string1{
 public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
-String a=sc.next();
+String a=sc.nextLine();
 System.out.println(a);
 char s[]=a.toCharArray();
-System.out.println(s[0]+""+s[4]);
+System.out.println("First and Fifth index:"+s[0]+""+s[4]);
+System.out.println("Reverse Of String:");
 for(int i=a.length()-1;i>=0;i--){
-System.out.println(s[i]);
+System.out.print(s[i]);
+}
+System.out.println("\n"+"Split():");
+
+String str1[]=a.split("e");
+for(String w:str1){
+System.out.println(w.trim());
 }
 }
 }
