eacbe3efc74ab5eea4813ca4c386d302377e3196
Vignesh <vigneshit13@gmail.com>
2018-03-12 06:23:44 +0530

    Java Imp Files

---
+++ Runner1.java
@@ -0,0 +1,11 @@
+package Linear;
+
+public class Runner1{
+public static void main(String args[]){
+LinkedList list=new LinkedList();
+
+list.insert(5698);
+list.insert(2509);
+list.show();
+}
+}
---
+++ linked.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class linked{
+public static void main(String args[]){
+LinkedList<String> ob=new LinkedList<String>();
+
+for(char a='a';a<'z';a++){
+ob.add(Character.toString(a));
+}
+
+System.out.println(ob);
+}
+}
